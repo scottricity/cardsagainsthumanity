5a2d011f3ec77aef302f3af0d562e73e3518753b
Timothy3773 <jonathanscott122@gmail.com>
2022-11-12 12:14:25 -0500

    Chnages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import * as React from "react"
+import { Box , Typography } from "@mui/material"
+
+function App() {
+  return (
+    <Box>
+      <Typography>
+        yes
+      </Typography>
+      
+    </Box>
+  );
+}
+
+export default App;
